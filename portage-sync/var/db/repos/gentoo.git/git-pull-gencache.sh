b83d8f919d1441426ac593afc050f32f0ef7d625
bekcpear <i@bitbili.net>
2021-06-02 18:15:07 +0800

    portage-sync/../git-pull-gencache.sh: verify unve-
    rified commits before a new sync started
    &
    others

--- portage-sync/var/db/repos/gentoo.git/git-pull-gencache.sh
+++ portage-sync/var/db/repos/gentoo.git/git-pull-gencache.sh
@@ -4,38 +4,79 @@
 # newly pulled commits. After than, regenerate the metadata cache
 # Takes no argument.
 
-WDIR=/var/db/repos/gentoo.git
+REPO_PATH="/var/db/repos/gentoo.git/gentoo"
+CONTINUED_COMMIT=/var/db/repos/gentoo.git/CONTINUED_COMMIT
+LAST_COMMIT=
 
-echo "cd /var/db/repos/gentoo.git/gentoo"
-cd /var/db/repos/gentoo.git/gentoo
+function verify-sign-until() {
+	local dir=${1}
+	shift
+	commits=( $(git rev-list "$1..HEAD") )
+	declare -i i=${#commits[@]}-1 len=0
+	for (( ; i > -1; i-- )); do
+		commit=${commits[i]}
+		msg=$(git --no-pager log -1 --format="%h %s << %ce" "$commit")
+		echo "Verifying $msg"
+		output=$(git verify-commit --raw "$commit" 2>&1)
+		res=$?
+		if [[ $res != 0 ]]; then
+			echo "Verification failed!"
+			git --no-pager log -1 --pretty=fuller "$commit"
+			echo "${output}"
+			if grep "\\[GNUPG:\\] EXPKEYSIG" <<< "$output"; then
+				echo "... but expired keys are alright..."
+			else
+				echo -n ${dir}:${LAST_COMMIT} >${CONTINUED_COMMIT}
+				return 1
+			fi
+		fi
+		LAST_COMMIT=${commit}
+	done
+	return 0
+}
 
-BEGIN_COMMIT=$(git rev-list -n1 master)
+echo "cd ${REPO_PATH}"
+eval "cd ${REPO_PATH}"
+
+if [[ -e ${CONTINUED_COMMIT} ]]; then
+	LAST_C=$(cat ${CONTINUED_COMMIT})
+	LAST_C_DIR=${LAST_C%:*}
+	LAST_COMMIT=${LAST_C##*:}
+	pushd ${LAST_C_DIR}
+	echo "Verifying unverified commits..."
+	eval "verify-sign-until ${LAST_C_DIR} ${LAST_COMMIT}"
+	if [[ $? != 0 ]]; then
+		exit 1
+	fi
+	popd
+fi
+
+LAST_COMMIT=$(git rev-list -n1 master)
+echo -n .:${LAST_COMMIT} >${CONTINUED_COMMIT}
 
 echo "Pulling..."
-#proxychains git pull
 git pull
 
 echo "Verifying..."
-${WDIR}/verif-sign-until.sh $BEGIN_COMMIT
+eval "verify-sign-until . ${LAST_COMMIT}"
 if [[ $? != 0 ]]; then
-	echo "BEGIN COMMIT: "${BEGIN_COMMIT}
 	exit 1
 fi
 
 for x in dtd glsa news xml-schema; do
 	pushd ./metadata/${x}
-	BEGIN_COMMIT=$(git rev-list -n1 master)
-	#proxychains git pull
+	LAST_COMMIT=$(git rev-list -n1 master)
+	echo -n ./metadata/${x}:${LAST_COMMIT} >${CONTINUED_COMMIT}
 	git pull
 	echo "Verifying ${x}..."
-	${WDIR}/verif-sign-until.sh $BEGIN_COMMIT
+	eval "verify-sign-until ./metadata/${x} ${LAST_COMMIT}"
 	if [[ $? != 0 ]]; then
-		echo "BEGIN COMMIT: "${BEGIN_COMMIT}
 		popd
 		exit 1
 	fi
 	popd
 done
+rm -f ${CONTINUED_COMMIT}
 
 echo "Regenerating cache..."
 egencache --jobs=30 --repo=gentoo --update --update-pkg-desc-index --update-use-local-desc -v
--- portage-sync/var/db/repos/gentoo.git/verif-sign-until.sh
+++ portage-sync/var/db/repos/gentoo.git/verif-sign-until.sh
@@ -8,20 +8,26 @@
 
 # TODO: use trust levels to error out on untrusted keys
 
-for commit in $(git rev-list "$1..HEAD"); do
-    msg=$(git --no-pager log -1 --format="%h %s << %ce" "$commit")
-    echo "Verifying $msg"
-    output=$(git verify-commit --raw "$commit" 2>&1)
-    res=$?
-    if [[ $res != 0 ]]; then
-        echo "Verification failed!"
-        git --no-pager log -1 --pretty=fuller "$commit"
-        echo "${output}"
-        if grep "\\[GNUPG:\\] EXPKEYSIG" <<< "$output"; then
-            echo "... but expired keys are alright..."
-        else
-            exit 1
-        fi
-    fi
+commits=( $(git rev-list "$1..HEAD") )
+declare -i i=${#commits[@]}-1 len=0
+
+for (( ; i > -1; i-- )); do
+	commit=${commits[i]}
+	len+=1
+	msg=$(git --no-pager log -1 --format="%h %s << %ce" "$commit")
+	echo "Verifying $msg"
+	output=$(git verify-commit --raw "$commit" 2>&1)
+	res=$?
+	if [[ $res != 0 ]]; then
+		echo "Verification failed!"
+		git --no-pager log -1 --pretty=fuller "$commit"
+		echo "${output}"
+		if grep "\\[GNUPG:\\] EXPKEYSIG" <<< "$output"; then
+			echo "... but expired keys are alright..."
+		else
+			exit 1
+		fi
+	fi
 done
+echo LEN:${len}
 exit 0
